e7c3dd3a4d7709f5a48c56dda50611a4cfa3aea8
Nikita Liakhovka <n.liakhovka@gmail.com>
2022-04-14 15:27:00 +0900

    updated to use Formspree

--- pages/help/email-us.js
+++ pages/help/email-us.js
@@ -1,9 +1,43 @@
-import { Container, Heading, Box, FormControl, FormLabel, Input, Textarea, Button } from '@chakra-ui/react';
+import { Container, Heading, Box, FormControl, Input, Textarea, Button, Link, Stack } from '@chakra-ui/react';
+import { default as NLink } from 'next/link';
 import Head from 'next/head';
+import { useForm, ValidationError } from '@formspree/react';
 import ko from '../../locales/ko/help/email-us.json';
 
 export default function Email() {
   const t = ko;
+  const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM);
+
+  if (state.succeeded) {
+    return (
+      <>
+        <Head>
+          <title>{t.title}</title>
+        </Head>
+        <Container maxW={'container.sm'} pt={'80px'} pb={'8'} h={'full'} display={'grid'} placeItems={'center'}>
+          <Stack
+            textAlign={'center'}
+            bg={'white'}
+            borderRadius={'md'}
+            px={'8'}
+            py={'4'}
+            shadow={'md'}
+            alignItems={'center'}
+          >
+            <Box fontSize={'3xl'}>🙏</Box>
+            <Box fontWeight={'bold'} fontSize={'xl'}>
+              {t.success.thanks}
+            </Box>
+            <NLink href={'/'} passHref>
+              <Link color={'blue'} w={'fit-content'}>
+                {t.success.goMain}
+              </Link>
+            </NLink>
+          </Stack>
+        </Container>
+      </>
+    );
+  }
   return (
     <>
       <Head>
@@ -22,26 +56,24 @@ export default function Email() {
           borderRadius={'md'}
           shadow={'md'}
           p={'8'}
-          name={'contact'}
-          method={'POST'}
-          data-netlify={true}
-          data-netlify-honeypot={'bot-field'}
-          action={'/help/success'}
+          onSubmit={handleSubmit}
         >
-          <input type='hidden' name='form-name' value='contact' />
           <FormControl isRequired>
             <label htmlFor='email'>{t.email}</label>
             <Input mt={'1'} id={'email'} type={'email'} name={'email'} />
+            <ValidationError prefix='Email' field='email' errors={state.errors} />
           </FormControl>
           <FormControl isRequired>
             <label htmlFor='name'>{t.name}</label>
             <Input mt={'1'} id={'name'} type={'text'} name={'name'} />
+            <ValidationError prefix='Name' field='name' errors={state.errors} />
           </FormControl>
           <FormControl isRequired>
             <label htmlFor='message'>{t.message}</label>
             <Textarea mt={'1'} id={'message'} resize={'none'} name={'message'} />
+            <ValidationError prefix='Message' field='message' errors={state.errors} />
           </FormControl>
-          <Button type={'submit'} colorScheme={'purple'}>
+          <Button type={'submit'} colorScheme={'purple'} disabled={state.submitting}>
             {t.submit}
           </Button>
         </Box>
