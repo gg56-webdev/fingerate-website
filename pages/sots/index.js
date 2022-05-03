c77d31939121f9d39e2ea9ef9d4c3c4bd1367e9e
Nikita Liakhovka <n.liakhovka@gmail.com>
2022-05-03 17:31:24 +0900

    update sots

--- pages/sots/index.js
+++ pages/sots/index.js
@@ -13,11 +13,17 @@ import {
   Checkbox,
   Switch,
   FormLabel,
+  LinkBox,
+  LinkOverlay,
+  FormControl,
+  Skeleton,
 } from '@chakra-ui/react';
 import { db } from '../../lib/firebase';
 import { getDocs, collection, where, query, limit, startAfter, orderBy, documentId } from 'firebase/firestore';
+import useMapFilter from '../../hooks/useMapFilter';
 import Image from 'next/image';
 import Head from 'next/head';
+import { default as NLink } from 'next/link';
 
 import ko from '../../locales/ko/sots.json';
 import en from '../../locales/en/sots.json';
@@ -28,14 +34,33 @@ import { ArrowDownIcon, ArrowUpIcon, RepeatIcon } from '@chakra-ui/icons';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const MotionGrid = motion(Grid);
-const MotionStack = motion(Stack);
+const MotionLinkBox = motion(LinkBox);
 
-export default function Sots({ sots }) {
-  const [filteredSots, setFilteredSots] = useState(sots);
-  const [grade, setGrade] = useState(null);
-  const [country, setCountry] = useState(null);
-  const [ownership, setOwnership] = useState(false);
-  const [sort, setSort] = useState('');
+const LIMIT = 6 * 5;
+
+export async function getServerSideProps() {
+  const colRef = collection(db, 'sots');
+
+  const cq = query(
+    colRef,
+    //  where('owner', '==', '')
+    limit(LIMIT)
+  );
+  const snap = await getDocs(cq);
+
+  const sotsData = snap.docs.map((doc) => {
+    const { name, country, city, grade, image, price, owner } = doc.data();
+    return { name, country, city, grade, image, price, id: doc.id, owner };
+  });
+
+  return {
+    props: { sotsData },
+    // revalidate: 60 * 30,
+  };
+}
+
+export default function Sots({ sotsData }) {
+  const { arrayToFilter: sots, setGradeFilter, setCountryFilter, setForSaleFilter } = useMapFilter(sotsData);
 
   const [isLoading, setIsLoading] = useState(false);
   const [isCollectionEnd, setIsCollectionEnd] = useState(false);
@@ -81,173 +106,33 @@ export default function Sots({ sots }) {
       <Head>
         <title>SoTs</title>
       </Head>
-      <Container maxW={'1400px'} pt='70px' px='1' pb='8'>
-        <Stack p={'2'} bg='white' borderRadius={'md'}>
-          <Heading as='h1' textAlign='center'>
-            SoTs
-          </Heading>
-          <Box overflowX={'auto'} position='sticky' top={'65px'} zIndex={'2'}>
-            <Stack direction={'row'} color='common.main' p={2} borderRadius='md' bg='blue.100' w={'max-content'}>
-              <Box bg='white' borderRadius={'md'}>
-                <Input
-                  list='countries'
-                  onChange={(e) => setCountry(e.target.value)}
-                  placeholder={t.filters.country}
-                  value={country || ''}
-                  h='full'
-                />
+      <Container maxW={'1400px'} pt='80px' px='2' pb='8'>
+        <Heading as='h1' textAlign='center'>
+          SoTs
+        </Heading>
+        <Stack p={'2'} bg='white' borderRadius={'md'} shadow='md'>
+          <Filters
+            availableCountries={availableCountries}
+            content={t}
+            setGradeFilter={setGradeFilter}
+            setForSaleFilter={setForSaleFilter}
+            setCountryFilter={setCountryFilter}
+          />
 
-                <datalist id='countries'>
-                  {availableCountries.map((c, i) => (
-                    <option value={c} key={c + i} />
-                  ))}
-                </datalist>
-              </Box>
-              <Box bg='white' borderRadius={'md'}>
-                <Select
-                  variant='outline'
-                  onChange={(e) => setGrade(e.target.value)}
-                  placeholder={t.filters.grade}
-                  value={grade || ''}>
-                  {['S', 'A', 'B', 'C', 'D'].map((val) => (
-                    <option value={val} key={val}>
-                      {val}
-                    </option>
-                  ))}
-                </Select>
-              </Box>
-              {!grade && (
-                <Flex bg='white' flexDirection={'row'} alignItems='center' borderRadius={'md'} px={1}>
-                  <Button
-                    fontSize={'md'}
-                    fontWeight='normal'
-                    colorScheme={sort ? 'blue' : 'gray'}
-                    variant={sort ? 'solid' : 'ghost'}
-                    size={'sm'}
-                    onClick={() => (sort === 'desc' ? setSort('asc') : setSort('desc'))}
-                    rightIcon={sort && (sort === 'desc' ? <ArrowDownIcon /> : <ArrowUpIcon />)}>
-                    {t.filters.price}
-                  </Button>
-                </Flex>
-              )}
-              <Flex bg='white' borderRadius={'md'} alignItems='center' px='1'>
-                <Switch
-                  id='ownership-switch'
-                  fontSize={'md'}
-                  isChecked={ownership}
-                  onChange={() => setOwnership(!ownership)}
-                  mr='1'
-                />
-                <FormLabel htmlFor='ownership-switch' m='0'>
-                  {t.filters.ownership}
-                </FormLabel>
-              </Flex>
-              <Button
-                alignSelf={'center'}
-                colorScheme={'gray'}
-                onClick={() => {
-                  setCountry(null);
-                  setGrade(null);
-                  setOwnership(false);
-                  setSort('');
-                }}>
-                <RepeatIcon />
-              </Button>
-            </Stack>
-          </Box>
-          <MotionGrid gap='2' gridTemplateColumns={'repeat(auto-fill, minmax(200px, 1fr))'} layout>
+          {/* <MotionGrid
+            gap='2'
+            gridTemplateColumns={{
+              base: 'repeat(auto-fill, minmax(150px, 1fr))',
+              sm: 'repeat(auto-fill, minmax(200px, 1fr))',
+            }}
+            layout
+          >
             <AnimatePresence>
-              {filteredSots
-                .filter((sot) => (grade ? sot.grade.toLowerCase() === grade.toLowerCase() : true))
-                .filter((sot) => (country ? sot.country.toLowerCase() === country.toLowerCase() : true))
-                .filter((sot) => (ownership ? !sot.owner : true))
-                .sort((a, b) => (sort === 'desc' ? b.price - a.price : sort === 'asc' ? a.price - b.price : 0))
-                .map((sot) => (
-                  <MotionStack
-                    key={sot.id}
-                    shadow='md'
-                    borderRadius={'md'}
-                    bg='cyan.50'
-                    p='1'
-                    onClick={() =>
-                      router.push(`/sots/${sot.id}`, `/sots/${sot.id}`, {
-                        locale,
-                      })
-                    }
-                    transition='all 0.2s'
-                    _hover={{
-                      outline: '2px solid',
-                      outlineColor: 'common.main',
-                      cursor: 'pointer',
-                    }}
-                    border='1px solid'
-                    borderColor={'blue.100'}
-                    layout
-                    animate={{ opacity: 1 }}
-                    initial={{ opacity: 0 }}
-                    exit={{ opacity: 0 }}>
-                    <Box borderRadius={'md'} overflow='hidden'>
-                      <Image
-                        src={sot.image}
-                        alt={`Thumbnail of SoT${sot.id}`}
-                        width='400'
-                        height='400'
-                        layout='responsive'
-                      />
-                    </Box>
-                    <Stack spacing={1} flex='1' justifyContent='space-between'>
-                      <Stack spacing={1}>
-                        <Box as={'strong'} color='common.main' fontFamily={'sans-serif'} fontSize='100%'>
-                          {sot.name}
-                        </Box>
-                        <Box as={'small'} color='blue.400' fontFamily={'mono'}>{`SoT ${sot.id}`}</Box>
-                      </Stack>
-                      <Box as={'small'} color='blue'>
-                        <Icon>
-                          <path
-                            fill='currentColor'
-                            fillRule='evenodd'
-                            d='M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z'
-                            clipRule='evenodd'
-                          />
-                        </Icon>
-                        {sot.country}, {sot.city}
-                      </Box>
-                    </Stack>
-                    <Flex
-                      textAlign={'center'}
-                      fontWeight={'bold'}
-                      bg={sot.owner ? 'gray.100' : 'common.second'}
-                      p='1'
-                      borderRadius={'md'}
-                      alignItems='center'
-                      sx={{ gap: '0.5rem' }}>
-                      <Box
-                        bg={'white'}
-                        borderRadius='md'
-                        p='2'
-                        fontFamily={'sans-serif'}
-                        border='2px solid'
-                        borderColor={`grades.${sot.grade}`}>
-                        {sot.grade}
-                      </Box>
-                      <Text as={'span'} color={'common.main'}>
-                        {sot.owner ? (
-                          <>
-                            판매 완료{' '}
-                            <Text as={'span'} fontSize='sm' fontStyle={'italic'}>
-                              ($ {sot.price})
-                            </Text>
-                          </>
-                        ) : (
-                          `$ ${sot.price.toLocaleString()}`
-                        )}
-                      </Text>
-                    </Flex>
-                  </MotionStack>
-                ))}
+              {sots.map((sot) => (
+                <SotCard key={sot.id} sot={sot} t={t} />
+              ))}
             </AnimatePresence>
-          </MotionGrid>
+          </MotionGrid> */}
           {/* {!isCollectionEnd && (
             <Button
               w='fit-content'
@@ -265,25 +150,149 @@ export default function Sots({ sots }) {
   );
 }
 
-const LIMIT = 6 * 5;
+const SotCard = ({ sot, t }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  return (
+    <MotionLinkBox
+      display='flex'
+      flexDir='column'
+      borderRadius='md'
+      borderWidth='2px'
+      shadow='base'
+      bg='cyan.50'
+      p='1'
+      _hover={{
+        outline: '2px solid',
+        outlineColor: 'common.main',
+      }}
+      layout
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      sx={{ '& img': { borderRadius: 'md' } }}
+    >
+      <Box pos={'relative'} mb='2'>
+        <Image
+          src={sot.image}
+          alt={`Thumbnail of SoT${sot.id}`}
+          width='400'
+          height='400'
+          layout='responsive'
+          onLoad={() => setIsLoaded(true)}
+        />
+        {!isLoaded && <Skeleton pos='absolute' inset='0' />}
+      </Box>
 
-export async function getServerSideProps() {
-  const colRef = collection(db, 'sots');
-
-  const cq = query(
-    colRef,
-    //  where('owner', '==', '')
-    limit(LIMIT)
+      <Flex flexDir='column' flex='1'>
+        <NLink href={`sots/${sot.id}`} passHref>
+          <LinkOverlay mb='2'>
+            <Box
+              as='strong'
+              display='block'
+              color='common.main'
+              fontFamily='sans-serif'
+              fontWeight='bold'
+              fontSize={{ base: 'md', md: 'lg' }}
+            >
+              {sot.name}
+            </Box>
+          </LinkOverlay>
+        </NLink>
+        <Box
+          mt='auto'
+          as='small'
+          display={{ base: 'none', sm: 'block' }}
+          color='blue.400'
+          fontFamily={'mono'}
+        >{`SoT ${sot.id}`}</Box>
+        <Box as='small' display={{ base: 'none', sm: 'block' }} color='blue' mb='2'>
+          <Icon>
+            <path
+              fill='currentColor'
+              fillRule='evenodd'
+              d='M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z'
+              clipRule='evenodd'
+            />
+          </Icon>
+          {sot.country}, {sot.city}
+        </Box>
+      </Flex>
+      <Flex fontWeight={'bold'} sx={{ gap: 1 }} fontFamily={'sans-serif'}>
+        <Grid
+          textAlign='center'
+          placeItems='center'
+          borderRadius='md'
+          h='8'
+          w='8'
+          bg={`grades.${sot.grade}`}
+          color='white'
+          shadow='inner'
+          textShadow='-1px -1px 1px rgba(255,255,255,.1), 1px 1px 1px rgba(0,0,0,.5)'
+        >
+          {sot.grade}
+        </Grid>
+        <Text
+          color={sot.owner ? 'gray.600' : 'common.main'}
+          bg={sot.owner ? 'gray.100' : 'common.second'}
+          py='1'
+          px='2'
+          borderRadius={'md'}
+          flex='1'
+          shadow='inner'
+          fontStyle={sot.owner ? 'italic' : 'normal'}
+          fontWeight={sot.owner ? 'normal' : 'inherit'}
+          fontSize={'md'}
+        >
+          {sot.owner ? (
+            <>
+              <Text as='ins' textDecoration='none'>
+                {t.hasOwner}{' '}
+              </Text>
+              <Text as='span' fontSize='sm' display={{ base: 'none', sm: 'inline' }}>
+                ($ {sot.price})
+              </Text>
+            </>
+          ) : (
+            `${t.currency} ${sot.price.toLocaleString()}`
+          )}
+        </Text>
+      </Flex>
+    </MotionLinkBox>
   );
-  const snap = await getDocs(cq);
+};
 
-  const sots = snap.docs.map((doc) => {
-    const { name, country, city, grade, image, price, owner } = doc.data();
-    return { name, country, city, grade, image, price, id: doc.id, owner };
-  });
-
-  return {
-    props: { sots },
-    // revalidate: 60 * 30,
-  };
-}
+const Filters = ({ availableCountries, setCountryFilter, setGradeFilter, content: { filters }, setForSaleFilter }) => {
+  return (
+    <Flex sx={{ gap: 1 }} p='1' bg={'blue.50'} borderRadius='md'>
+      <Select bg='white' w='auto' onChange={(e) => setCountryFilter(e.target.value)} placeholder={filters.country}>
+        {availableCountries.map((country) => (
+          <option key={country} value={country}>
+            {country}
+          </option>
+        ))}
+      </Select>
+      <Select bg='white' w='auto' onChange={(e) => setGradeFilter(e.target.value)} placeholder={filters.grade}>
+        {['S', 'A', 'B', 'C', 'D'].map((g) => (
+          <option key={g} value={g}>
+            {g}
+          </option>
+        ))}
+      </Select>
+      <FormControl
+        bg='white'
+        display='flex'
+        alignItems='center'
+        borderWidth='1px'
+        borderColor='gray.200'
+        borderRadius='md'
+        px='2'
+        w='auto'
+      >
+        <FormLabel htmlFor='owner-toogle' m='0'>
+          {filters.ownership}
+        </FormLabel>
+        <Switch id='owner-toogle' onChange={(e) => setForSaleFilter(e.target.checked)} />
+      </FormControl>
+    </Flex>
+  );
+};
