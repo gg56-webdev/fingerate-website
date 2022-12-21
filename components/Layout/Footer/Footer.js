9afb0d8357f64f079e24fc4e7e3fae7cc4061c05
Nikita Liakhovka <n.liakhovka@gmail.com>
2022-12-21 14:22:22 +0900

    added translation for footer and eng docs

--- components/Layout/Footer/Footer.js
+++ components/Layout/Footer/Footer.js
@@ -11,11 +11,18 @@ import {
   AccordionPanel,
   AccordionIcon,
 } from '@chakra-ui/react';
-import { default as NLink } from 'next/link';
 import Image from 'next/image';
 import gg56logo from '../../../public/footer/gg56.svg';
 
+import en from '../../../locales/en/footer.json';
+import ko from '../../../locales/ko/footer.json';
+import { useRouter } from 'next/router';
+
 export default function Footer() {
+  const { locale } = useRouter();
+
+  const t = locale === 'ko' ? ko : en;
+
   return (
     <Box as={'footer'} bg='white' mt='auto' shadow='inner'>
       <Container maxW='container.xl' p='2'>
@@ -50,11 +57,11 @@ export default function Footer() {
                   }}
                 >
                   <Box display='inline-flex' flexWrap={{ base: 'wrap', md: 'nowrap' }}>
-                    <dt>상호명</dt>
-                    <dd>주식회사 지지56코리아</dd>
+                    <dt>{t.companyInfo.company.term}</dt>
+                    <dd>{t.companyInfo.company.description}</dd>
                   </Box>
                   <Box display='inline-flex' flexWrap={{ base: 'wrap', md: 'nowrap' }}>
-                    <dt>연락처</dt>
+                    <dt>{t.companyInfo.contact.term}</dt>
                     <dd>
                       <Link color='blue' href='tel:025566780'>
                         02-556-6780
@@ -62,11 +69,11 @@ export default function Footer() {
                     </dd>
                   </Box>
                   <Box display='inline-flex' flexWrap={{ base: 'wrap', md: 'nowrap' }}>
-                    <dt>사업자등록번호</dt>
+                    <dt>{t.companyInfo.businessNumber.term}</dt>
                     <dd>351-86-01750</dd>
                   </Box>
                   <Box display='inline-flex' flexWrap={{ base: 'wrap', md: 'nowrap' }}>
-                    <dt>Contact</dt>
+                    <dt>{t.companyInfo.email.term}</dt>
                     <dd>
                       <Link color='blue' href='mailto:admin@fingerate.world'>
                         admin@fingerate.world
@@ -75,21 +82,21 @@ export default function Footer() {
                   </Box>
 
                   <Box display='inline-flex' flexWrap={{ base: 'wrap', md: 'nowrap' }}>
-                    <dt>대표자</dt>
-                    <dd>김영군</dd>
+                    <dt>{t.companyInfo.ceo.term}</dt>
+                    <dd>{t.companyInfo.ceo.description}</dd>
                   </Box>
 
                   <Box display='inline-flex' flexWrap={{ base: 'wrap', md: 'nowrap' }}>
-                    <dt>사업자주소</dt>
-                    <dd>서울 강남구 강남대로 408 4층</dd>
+                    <dt>{t.companyInfo.address.term}</dt>
+                    <dd>{t.companyInfo.address.description}</dd>
                   </Box>
                 </AccordionPanel>
               </AccordionItem>
             </Accordion>
           </Stack>
           <Flex color='blue' gap='6' justify='center'>
-            <Link href='/docs/terms-kor.pdf'>서비스 이용약관</Link>
-            <Link href='/docs/privacy-kor.pdf'>개인정보처리방침</Link>
+            <Link href={t.links.terms.link}>{t.links.terms.name}</Link>
+            <Link href={t.links.privacy.link}>{t.links.privacy.name}</Link>
           </Flex>
         </Flex>
         <Divider mt='1' />
