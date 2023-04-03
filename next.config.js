6a6a3a0100f4739a39b9879715b7dbd594e49c66
Nikita Liakhovka <n.liakhovka@gmail.com>
2023-04-03 10:35:56 +0900

    added team page

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ module.exports = {
   reactStrictMode: true,
   i18n: {
     locales: ['ko', 'en'],
-    defaultLocale: 'ko',
+    defaultLocale: 'en',
+    localeDetection: false,
   },
   images: {
     domains: ['res.cloudinary.com'],
---
+++ pages/team.js
@@ -0,0 +1,40 @@
+import { Container, Flex, Heading, Stack, Text } from '@chakra-ui/react';
+
+import ko from '../locales/ko/team.json';
+import en from '../locales/en/team.json';
+
+const Team = ({ locale }) => {
+  const t = locale === 'ko' ? ko : en;
+  return (
+    <Container textAlign='center' pt='65px'>
+      <Heading as='h1'>{t.h1}</Heading>
+      <Text>{t.subtitle}</Text>
+      <Grid></Grid>
+    </Container>
+  );
+};
+
+export default Team;
+
+const Profile = ({ person }) => {
+  const { name, subtitles, socialLinks } = person;
+  return (
+    <Stack>
+      <Image />
+      <Box as='strong'>{name}</Box>
+      {subtitles.length > 0 &&
+        subtitles.map((s, i) => (
+          <Text key={i} as='small'>
+            {s}
+          </Text>
+        ))}
+      {socialLinks.length > 0 && (
+        <Flex gap='2'>
+          {socialLinks.map((s, i) => (
+            <div key={i}>{s}</div>
+          ))}
+        </Flex>
+      )}
+    </Stack>
+  );
+};
